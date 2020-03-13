c957944920d01941ee4c4954db9d4bee281457d0
Pedro <35156354+pnava589@users.noreply.github.com>
2020-03-12 18:58:17 -0600

    added more socket functionality

--- stocks-api.js
+++ stocks-api.js
@@ -6,14 +6,6 @@ const stockRouter = require('./scripts/stock-router');
 const server = require('socket.io');
 
 
-
-
-/*// for now, we will get our data by reading the provided json file
-const jsonPath = path.join(__dirname, 'public',
-'stocks-complete.json');
-const jsonData = fs.readFileSync(jsonPath, 'utf8');
-// convert string data into JSON object
-const stocks = JSON.parse(jsonData);*/
 // create an express app
 const app = express();
 // tell node to use json and HTTP header features in body-parser
@@ -28,55 +20,44 @@ app.use('/socket.io', express.static(path.join(__dirname,'/node_modules/socket.i
 const io = new server(3000);
 const list =[];
 io.on('connection', socket => {
+  
       console.log('new connection made with client');
       
 
         socket.broadcast.on('new user',user=>{
           //console.log(user.results[0].name.first);
-          const name = user.results[0].name.first;
-          const rawGender = user.results[0].gender;
-          var gender = '';
-          if(rawGender=="male"){gender = 'men';}
-          else {gender = 'women';}
-          const id = Math.floor(Math.random() * 70) + 1;
-          const obj = {Name:name,Id:id,Gender:gender};
-          list.push(obj);
-          io.emit('user joined',list);
-          console.log(list);
+            socket.username = user.results[0].name.first;
+            const rawGender = user.results[0].gender;
+            var gender = '';
+            if(rawGender=="male"){gender = 'men';}
+            else {gender = 'women';}
+            const id = Math.floor(Math.random() * 70) + 1;
+            const obj = {Name:socket.username ,Id:id,Gender:gender};
+            list.push(obj);
+            const newobj = {users:list,user:obj};
+            io.emit('user joined',newobj);
+          console.log(newobj.users);
 
-        
-        /*socket.username =msg;
-        const obj = {message:"Has joined",user:msg};
-        io.emit('user joined',obj);
+        });
+
+        socket.on('client leaving',id=>{
+          const user = list.find(element => element.Id == id);
+          const index = list.indexOf(user);
+          io.emit("user left",user);
+          list.splice(index,1);
+          
+
         });
 
         socket.on('chat from client',msg=>{
-          io.emit('chat from server',{user:socket.username,message:msg});*/
+          socket.broadcast.emit('chat from server',{user:socket.username,message:msg});
+        
         });
       
     
     });
 
 
-// return all the stocks when a root request arrives
-//app.route('/')
-  /*  app.get('/stock/:symbol', (req,resp) => {
-    
-        const symbolToFind = req.params.symbol.toUpperCase();
-        const matches = stocks.filter(obj => symbolToFind === obj.symbol);
-        resp.json(matches);
-     });
-
-     // return all the stocks whose name contains the supplied text
-            app.get('/stock/name/:substring', (req,resp) => {
-            // change user supplied substring to lower case
-            const substring = req.params.substring.toLowerCase();
-            // search the array of objects for a match
-            const matches = stocks.filter( (obj) => obj.name.toLowerCase().includes(substring) );
-            // return the matching stocks
-            resp.json(matches);
-    });*/
-    // Use express to listen to port
     let port = 8080;
     app.listen(port, () => {
     console.log("Server running at port= " + port);
